54add43082dc3689b5d43b844c2da646a6eab221
prateek.dubey02 <prateek.dubey02@infosys.com>
2024-11-22 15:39:59 +0530

    Add swagger api name

--- src/main/java/com/infosys/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/infosys/taskmanager/TaskManagerApplication.java
@@ -1,9 +1,13 @@
 package com.infosys.taskmanager;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Task Manager Application",
+description = "Task management system where team members can create, track, and manage their daily tasks."))
 public class TaskManagerApplication {
 
 	public static void main(String[] args) {
